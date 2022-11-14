b75b1c0fa144cb390bb06de285c946b547419211
netsky_prod <arenda244@yandex.ru>
2022-11-14 13:59:28 +0300

    init

---
+++ lib/tinkoff_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tinkoff_client/version"
+
+module TinkoffClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tinkoff_client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TinkoffClient do
+  it "has a version number" do
+    expect(TinkoffClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
