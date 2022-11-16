108c5b8afde2de6cfbb4f559e81231380aace4db
netsky-dev <arenda244@yandex.ru>
2022-11-16 05:19:32 +0300

    update generator

--- lib/tinkoff_client/generators/templates/tinkoff_client_template.rb
+++ lib/tinkoff_client/generators/templates/tinkoff_client_template.rb
@@ -2,4 +2,9 @@ TinkoffClient.configure do |c|
   #c.payment_public_key = "./public.key"
   c.payment_terminal_key = ENV["TERMINAL_KEY"]
   c.payment_terminal_secret = ENV["TERMINAL_SECRET"]
+
+  c.payout_terminal_key = ENV["PAYOUT_TERMINAL_KEY"]
+  c.payout_terminal_secret = ENV["PAYOUT_TERMINAL_SECRET"]
+  c.payout_certificate = "./open-api-cert.pem"
+  c.payout_private_key = "./private.key"
 end
